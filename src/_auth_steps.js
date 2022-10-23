2a6fc2bd6abb742310bea964288414911dafc5e4
nasirmondol <nasirahmed8202@gmail.com>
2022-10-23 09:15:36 +0600

    firebase auth initialize

--- src/_auth_steps.js
+++ src/_auth_steps.js
@@ -1,6 +1,6 @@
 /***
  * 1.console.google.firebase.com and create a firebase project.
  * 2.npm install firebase in my ema-john-projects.
- * 3.create afirebase.init.js folder in src and then setup the sdk file.
+ * 3.create a firebase.init.js folder in src and then setup the sdk file.
  * 
  */
